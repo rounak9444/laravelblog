910d860a34cf04c6f0e7cb0cba9eb4534877a417
Rounak <rounak9444@outlook.com>
2021-08-26 10:35:18 +0530

    added show method in post controller and created a view to show a particular post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -65,12 +65,12 @@ class PostController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int  $slug
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($slug)
     {
-        //
+        return view('blog.show')->with('post' Post::where('slug', $slug)->first());
     }
 
     /**
